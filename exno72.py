739074227318515e12cbb5da78fcfd540f0d6da1
rsowmyasowmi <41049079+rsowmyasowmi@users.noreply.github.com>
2019-01-08 15:16:27 +0530

    Add files via upload

--- exno72.py
+++ exno72.py
@@ -1,8 +1,8 @@
 st=input("Enter any String:")
 vow=['a','e','i','o','u','A','E','I','O','U']
 flag=0
-for x in st:
-	if x in vow:
+for i in st:
+	if i in vow:
 		flag=1
 if(flag==1):
 	print("Vowel is present")
