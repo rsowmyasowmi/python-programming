13235ff44b133e457c24391aa07e156c4e72d36d
rsowmyasowmi <41049079+rsowmyasowmi@users.noreply.github.com>
2019-01-11 10:41:35 +0530

    Add files via upload

--- exno77.py
+++ exno77.py
@@ -1,6 +1,6 @@
-k=int(input("enter the no"))
-for i in range(1,k+1):
-    if(k%i==0):
+N=int(input("enter the no"))
+for i in range(1,N+1):
+    if(N%i==0):
         print(i,end=',')
     else:
         print("invalid")
