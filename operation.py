fbb819163218f5ffbac596008198941857fdc7bb
rsowmyasowmi <41049079+rsowmyasowmi@users.noreply.github.com>
2019-01-11 13:48:24 +0530

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,14 @@
+n=input("Enter your seqence (Mod/Dividee):")
+op=['%','/']
+for i in n :
+	if i in op:
+		if(i=='%'):
+			k1=int(n.split(i)[0])
+			k2=int(n.split(i)[1])
+			ans=k1%k2
+		elif(i=='/'):
+			k1=int(n.split(i)[0])
+			k2=int(n.split(i)[1])
+			ans=k1//k2
+print(ans)
+
